3cde75b82c9125fa2784d8fb5a094508e0c6768a
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-13 20:32:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
+def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "phrase"
